d7a74037ec640477efcb914554e15d236d53c18f
Sophia Filipovic <103005707+sophiafil@users.noreply.github.com>
2022-09-29 19:23:44 -0400

    modified:   src/app/app.component.html 	modified:   src/app/app.component.scss 	modified:   src/app/components/view-donation/view-donation.component.html 	modified:   src/app/components/view-donation/view-donation.component.scss 	modified:   src/app/components/view-donation/view-donation.component.ts

--- src/app/components/view-donation/view-donation.component.ts
+++ src/app/components/view-donation/view-donation.component.ts
@@ -1,23 +1,25 @@
 import {Component} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface Donations {
   itemName: string;
   donor: string;
   program: string;
   donationDate: string;
+  numOfItem: number;
 }
 
 const ELEMENT_DATA: Donations[] = [
-  {itemName: 'Canned Black Beans', donor: 'Publix', program: 'FOOD_PANTRY', donationDate: '9/01/2022'},
-  {itemName: 'Always Maxi Pads Overnight Absorbency Unscented without Wings', donor: 'Target', program: "PERIOD_PROGRAM", donationDate: '08/17/2022'},
-  {itemName: 'Sassy Disposable Diaper Bags', donor: 'Walmart', program: 'DIAPER_DRIVE', donationDate: '9/16/2022'},
-  {itemName: "Parent's Choice Fragrance Free Baby Wipes", donor: 'Walmart', program: 'DIAPER_DRIVE', donationDate: '9/16/2022'},
-  {itemName: 'Tampax Pearl Ultra Tampons', donor: 'Target', program: 'PERIOD_PROGRAM', donationDate: '08/17/2022'},
-  {itemName: 'French Baguette Loaf', donor: 'Panera', program: "FOOD_PANTRY", donationDate: '9/30/2022'},
-  {itemName: 'Plain Bagel', donor: 'Panera', program: "FOOD_PANTRY", donationDate: '9/30/2022'},
-  {itemName: 'Classic Sourdough Loaf', donor: 'Panera', program: 'FOOD_PANTRY', donationDate: '9/30/2022'},
-  {itemName: 'AZO Complete Feminine Balance', donor: 'Target', program: "PERIOD_PROGRAM", donationDate: '9/20/2022'},
-  {itemName: 'Gala Apples', donor: 'Publix', program: 'FOOD_PANTRY', donationDate: '9/30/2022'},
+  {itemName: 'Canned Black Beans', donor: 'Publix', program: 'FOOD_PANTRY', donationDate: '9/01/2022', numOfItem: 20},
+  {itemName: 'Always Maxi Pads Overnight Absorbency Unscented without Wings', donor: 'Target', program: "PERIOD_PROGRAM", donationDate: '08/17/2022', numOfItem: 50},
+  {itemName: 'Sassy Disposable Diaper Bags', donor: 'Walmart', program: 'DIAPER_DRIVE', donationDate: '9/16/2022', numOfItem: 30},
+  {itemName: "Parent's Choice Fragrance Free Baby Wipes", donor: 'Walmart', program: 'DIAPER_DRIVE', donationDate: '9/16/2022', numOfItem: 25},
+  {itemName: 'Tampax Pearl Ultra Tampons', donor: 'Target', program: 'PERIOD_PROGRAM', donationDate: '08/17/2022', numOfItem: 50},
+  {itemName: 'French Baguette Loaf', donor: 'Panera', program: "FOOD_PANTRY", donationDate: '9/30/2022', numOfItem: 15},
+  {itemName: 'Plain Bagel', donor: 'Panera', program: "FOOD_PANTRY", donationDate: '9/30/2022', numOfItem: 20},
+  {itemName: 'Classic Sourdough Loaf', donor: 'Panera', program: 'FOOD_PANTRY', donationDate: '9/30/2022', numOfItem: 20},
+  {itemName: 'AZO Complete Feminine Balance', donor: 'Target', program: "PERIOD_PROGRAM", donationDate: '9/20/2022', numOfItem: 40},
+  {itemName: 'Gala Apples', donor: 'Publix', program: 'FOOD_PANTRY', donationDate: '9/30/2022', numOfItem: 45},
 ];
 
 @Component({
@@ -28,6 +30,10 @@ const ELEMENT_DATA: Donations[] = [
 
 
 export class Example {
-  displayedColumns: string[] = ['itemName', 'donor', 'program', 'donationDate'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['itemName', 'donor', 'program', 'donationDate', 'numOfItem'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(filterValue: String){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
