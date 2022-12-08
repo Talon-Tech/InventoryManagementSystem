f22b382a39cbe62bfa2cf2441b146a8bb9f50dc5
tayj0016 <tayj0016@gmail.com>
2022-12-08 17:20:46 -0500

    add firebase to vendor

--- src/app/services/vendorsvc.service.ts
+++ src/app/services/vendorsvc.service.ts
@@ -28,7 +28,7 @@ import { DbService } from 'src/app/services/db.service';
 })
 export class VendorsvcService {
   dbRef = this.dbSvc.getDb();
-  // vendorCollection = collection(this.dbSvc.getDb(), "vendors");
+  vendorCollection = collection(this.dbSvc.getDb(), "vendors");
 
   constructor(private router: Router, private dbSvc: DbService) { }
 
@@ -42,69 +42,27 @@ export class VendorsvcService {
     return vendors;
   }
 
-  async getVendorsObs() {
-    let vendors: DocumentData[] = [];
-
-    const q = query(collection(this.dbRef, "vendors"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      // vendors = []; 
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === "added") {
-          // console.log("New city: ", change.doc.data());
-          vendors.push({ ...change.doc.data() });
-        }
-        if (change.type === "modified") {
-          // console.log("Modified city: ", change.doc.data());
-        }
-        if (change.type === "removed") {
-          this.DeleteVendor(change.doc.id);
-
-          console.log(vendors.findIndex((element) => element['id'] = change.doc.id))
-          // console.log("Removed city: ", change.doc.data());
-        }
-      });
-    });
-
-    // const q = query(collection(this.dbRef, "vendors"));
-    // // vendors = []; 
-    // const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    //   let temp: any[] = [];
-    //   // vendors = []; 
-    //   querySnapshot.forEach((doc) => {
-    //     // vendors = []; 
-    //     temp.push({...doc.data()});
-    //     // console.log(temp); 
-    //   });
-    //   console.log(temp); 
-    //   vendors = temp; 
-    // });
-
-    // console.log(vendors);
-    // return querySnapshot.docs; 
-    return vendors;
-  }
-
   async GetVendor(id: any) {
-    // const docRef = doc(this.dbRef, "vendors", id);
-    // const docSnap = await getDoc(docRef);
+    const docRef = doc(this.dbRef, "vendors", id);
+    const docSnap = await getDoc(docRef);
 
-    // return docSnap.data();
+    return docSnap.data();
   }
 
   async AddVendor(vendor: Vendor) {
-    // const docRef = await addDoc(this.vendorCollection,
-    //   {
-    //     // id: uuidv4(),
-    //     name: vendor.name,
-    //     contactFirst: vendor.contactFirst,
-    //     contactLast: vendor.contactLast,
-    //     contactPhone: vendor.contactPhone,
-    //   },
+    const docRef = await addDoc(this.vendorCollection,
+      {
+        // id: uuidv4(),
+        name: vendor.name,
+        contactFirst: vendor.contactFirst,
+        contactLast: vendor.contactLast,
+        contactPhone: vendor.contactPhone,
+      },
 
-    // );
-    // await updateDoc(docRef, {
-    //   id: docRef.id,
-    // });
+    );
+    await updateDoc(docRef, {
+      id: docRef.id,
+    });
   }
 
   async addVendorTest() {
@@ -124,13 +82,13 @@ export class VendorsvcService {
   }
 
   async UpdateVendor(vendor: Vendor) {
-    // const vendorRef = doc(this.dbRef, "vendors", vendor.id);
-    // await updateDoc(vendorRef, {
-    //   name: vendor.name,
-    //   contactFirst: vendor.contactFirst,
-    //   contactLast: vendor.contactLast,
-    //   contactPhone: vendor.contactPhone,
-    // });
+    const vendorRef = doc(this.dbRef, "vendors", vendor.id);
+    await updateDoc(vendorRef, {
+      name: vendor.name,
+      contactFirst: vendor.contactFirst,
+      contactLast: vendor.contactLast,
+      contactPhone: vendor.contactPhone,
+    });
   }
 
   async DeleteVendor(id: any) {
